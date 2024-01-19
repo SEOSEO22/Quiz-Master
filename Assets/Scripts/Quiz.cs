adfb32f0505694bb6d5396f09c93a2bf1a609942
Cha Seoyeon <seoyeon0833@gmail.com>
2024-01-19 15:14:52 +0900

    질문의 답변을 TMPro의 텍스트와 연결

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -7,9 +7,17 @@ public class Quiz : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] QuestionSO question;
+    [SerializeField] GameObject[] answerButton;
+    TextMeshProUGUI answerText;
 
     private void Start()
     {
         questionText.text = question.GetQuestion();
+
+        for (int i = 0; i < answerButton.Length; i++)
+        {
+            answerText = answerButton[i].GetComponentInChildren<TextMeshProUGUI>();
+            answerText.text = question.GetAnswer(i);
+        }
     }
 }
