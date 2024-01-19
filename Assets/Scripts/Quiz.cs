0795311905727ffbf83ea7da47d1f33d96a96675
Cha Seoyeon <seoyeon0833@gmail.com>
2024-01-19 23:05:01 +0900

    중복 답안 체크 불가 기능 추가
    한 번 답을 입력한 질문의 경우 다시 버튼을 체크할 수 없도록 수정.
    다음 질문으로 넘어갈 경우 버튼이 다시 활성화되고 모두 기본 스프라이트로
    세팅되도록 설정.

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
 
 public class Quiz : MonoBehaviour
 {
@@ -12,9 +13,24 @@ public class Quiz : MonoBehaviour
     [SerializeField] Sprite correctAnswerSprite;
     [SerializeField] Sprite defaultAnswerSprite;
     int answerIndex;
+    Image buttonImage;
     TextMeshProUGUI answerText;
 
     private void Start()
+    {
+        GetNextQuestion();
+    }
+
+    // 다음 질문을 설정하는 메서드
+    void GetNextQuestion()
+    {
+        SetButtonState(true);
+        SetDefaultButtonSprites();
+        DesplayQuestion();
+    }
+
+    // 질문과 답변 텍스트를 설정하는 메서드
+    void DesplayQuestion()
     {
         questionText.text = question.GetQuestion();
 
@@ -25,10 +41,11 @@ public class Quiz : MonoBehaviour
         }
     }
 
+    // 정답/오답 선택 시 상호작용을 나타내는 메서드
     public void OnAnswerSelected(int index)
     {
         answerIndex = question.GetCorrectAnswerIndex();
-        Image buttonImage = answerButton[index].GetComponent<Image>();
+        buttonImage = answerButton[index].GetComponent<Image>();
 
         if (index == answerIndex)
         {
@@ -42,6 +59,28 @@ public class Quiz : MonoBehaviour
             buttonImage.color = new Color32(255, 130, 130, 120);
             buttonImage = answerButton[answerIndex].GetComponent<Image>();
             buttonImage.sprite = correctAnswerSprite;
+        }
+
+        SetButtonState(false);
+    }
+
+    // 버튼을 비활성화 상태로 만드는 메서드
+    void SetButtonState(bool state)
+    {
+        for (int i = 0; i < answerButton.Length; i++)
+        {
+            Button button = answerButton[i].GetComponent<Button>();
+            button.interactable = state;
+        }
+    }
+
+    // 답변 버튼의 스프라이트를 기본 스프라이트로 세팅하는 메서드
+    void SetDefaultButtonSprites()
+    {
+        for (int i = 0; i < answerButton.Length; i++)
+        {
+            buttonImage = answerButton[i].GetComponent<Image>();
+            buttonImage.sprite = defaultAnswerSprite;
         }
     }
 }
